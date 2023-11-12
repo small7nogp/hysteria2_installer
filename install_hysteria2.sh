27f979115b310f2f12774d32bc3c8a7fc0d4f29f
small7nogp <149787690+small7nogp@users.noreply.github.com>
2023-11-12 14:19:39 +0800

    Update install_hysteria2.sh

--- install_hysteria2.sh
+++ install_hysteria2.sh
@@ -32,8 +32,8 @@ CONFIG_DIR="/etc/hysteria"
 # URLs of GitHub
 # REPO_URL="https://github.com/apernet/hysteria"
 # API_BASE_URL="https://api.github.com/repos/apernet/hysteria"
-REPO_URL="https://github.com/small7nogp/h-y-s-t-e-r-i-a"
-API_BASE_URL="https://api.github.com/repos/small7nogp/h-y-s-t-e-r-i-a"
+REPO_URL="https://github.com/small7nogp/hysteria"
+API_BASE_URL="https://api.github.com/repos/small7nogp/hysteria"
 
 # curl command line flags.
 # To using a proxy, please specify ALL_PROXY in the environ variable, such like:
